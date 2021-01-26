db644153160c463ab974af8a2abdd1a11450770d
Karishmakarkii <karkikarishma143@gmail.com>
2021-01-26 21:05:37 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = '2703152e';
+  return(
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App;

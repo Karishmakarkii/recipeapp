b550fcb8f1ca323fcadbeb3997d3a167f7f0d320
Karishmakarkii <karkikarishma143@gmail.com>
2021-01-30 22:33:01 +0545

    last commit

--- src/App.js
+++ src/App.js
@@ -8,33 +8,50 @@ const App = () => {
   const APP_KEY = "36016ffc476522ec6d652a09e235e965 ";
 
   const[recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
   useEffect( ()=>{
-   getRecipes()
-  } , []);
+   getRecipes();
+  } , [query ]);
   const getRecipes = async () =>{
   
-  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data =  await response.json();
   setRecipes(data.hits);
   console.log(data.hits);
 
+  };
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    
+  };
+
+  const getSearch = e =>{
+    e.preventDefault(); 
+    setQuery(search);
+    setSearch('');
   }
   return(
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit= {getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button 
          className="search-button" 
          type="submit">
          Search</button>
       </form>
+      <div className="recipes">
       {recipes.map(recipe => (
-       <Recipe title={recipe.recipe.label} 
+       <Recipe
+       key = {recipe.recipe.label}
+        title={recipe.recipe.label} 
        calories={recipe.recipe.calories}
        image={recipe.recipe.image}
+       ingredients={recipe.recipe.ingredients}
        />
       ))};
+      </div>
      
     </div>
   )
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import style from './recipe.module.css';
 
 
-
-const Recipe = ({title,calories,image}) => {
+const Recipe = ({title,calories,image,ingredients}) => {
     return(
-        <div>
-        <h1>{title}</h1>
+        <div className={style.recipe}>
+        <h1 >{title}</h1>
+        <ol>
+            {ingredients.map(ingredient =>(
+                <li>{ingredient.text}</li>
+            ))}
+        </ol>
         <p>{calories}</p>
-        <img src={image}/>
+        <img className={style.image} src={image} alt = ""/>
+
         </div>
 
     );
